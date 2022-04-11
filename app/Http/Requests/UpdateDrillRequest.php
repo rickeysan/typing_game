31de18158011ca62bb4e6131bfab5b77c6ea90e5
rickeysan <rickeysan95@gmail.com>
2022-04-11 17:24:27 +0900

    デバッガーを削除

--- app/Http/Requests/UpdateDrillRequest.php
+++ app/Http/Requests/UpdateDrillRequest.php
@@ -24,20 +24,12 @@ class UpdateDrillRequest extends FormRequest
      */
     public function rules()
     {
-        \Debugbar::debug('ruleです');
-        \Debugbar::debug($this->request->all());
-        \Debugbar::debug(array_keys($this->request->all()));
-
         $fl_array = preg_grep("!problem[1-9]|[10]!", array_keys($this->request->all()));
-        \Debugbar::debug($fl_array);
 
         $rule_list = ['title' => ['required', 'max:60']];
         for ($i = 1; $i <= count($fl_array); $i++) {
-            \Debugbar::debug($i . '回目です');
-
             $rule_list['problem' . $i] = ['required', 'max:60', new AlphaRule];
         }
-        \Debugbar::debug($rule_list);
 
         return $rule_list;
     }
